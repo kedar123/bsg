eb833c5f0497bebffe6be57803bc0eb64a6263a9
kedar <kedar.pathak@pragtech.co.in>
2011-07-06 17:27:24 +0530

    removed the print statement from timing controller. need to test on servr

--- current/vendor/engines/gallery_back/app/controllers/admin/timings_controller.rb
+++ current/vendor/engines/gallery_back/app/controllers/admin/timings_controller.rb
@@ -13,8 +13,7 @@ class Admin::TimingsController < Admin::ApplicationController
 	def index
 		if params[:event]
 			@timings = Timing.all(:conditions =>"objectable_type = '#{params[:event]}' and objectable_id is not null")
-      p "this is timing" 
-      p @timings
+
    	else
 			@timings = Timing.all
 		end
