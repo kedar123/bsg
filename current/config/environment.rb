8161f8ad0382f21dc52cdf52dca7d07ede15a837
kedar pathak <kedar.pathak@pragtech.co.in>
2011-12-02 20:03:36 +0530

    committing for testing purpose cc thought works

--- current/config/environment.rb
+++ current/config/environment.rb
@@ -7,7 +7,7 @@
 
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.12' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 ##
 
