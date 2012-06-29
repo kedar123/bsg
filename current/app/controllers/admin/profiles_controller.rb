58ffd8cf9e46bdd7ddb9724f041237cfdb213983
kedar pathak <kedar.pathak@pragtech.co.in>
2012-06-29 15:04:37 +0530

    merged

--- current/app/controllers/admin/profiles_controller.rb
+++ current/app/controllers/admin/profiles_controller.rb
@@ -154,9 +154,11 @@ class Admin::ProfilesController < Admin::ApplicationController
     p id_list
     p "the total listtttt"
     p id_list.join(',')
-  
-    p 
-    @all_comp_art_work = ArtworksCompetition.find(:all,:conditions=>["competitions_users_id in (#{id_list.join(',')})"])
+    @all_comp_art_work = []
+     
+    if !id_list.join(',').blank?
+       @all_comp_art_work = ArtworksCompetition.find(:all,:conditions=>["competitions_users_id in (#{id_list.join(',')})"])
+    end 
     p @all_comp_art_work.length
    
     #@messages.flatten!
