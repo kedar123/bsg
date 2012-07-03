3bcc4cb534ba0cbcfd200de2f6887b339a815e64
kedar pathak <kedar.pathak@pragtech.co.in>
2012-07-03 12:58:03 +0530

    just put the conditions

--- current/app/controllers/admin/artworks_controller.rb
+++ current/app/controllers/admin/artworks_controller.rb
@@ -1,8 +1,8 @@
 class Admin::ArtworksController < ApplicationController
   
   def index
-    @current_objects = Artwork.find(:all)
-    @all_comp_art_work = ArtworksCompetition.find(:all)
+    @current_objects = Artwork.find(:all,:limit=>10)
+    @all_comp_art_work = ArtworksCompetition.find(:all,:limit=>10)
     render  :layout=> "gallery_promoting"
   end
   
