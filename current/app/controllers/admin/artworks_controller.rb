94e3074bc89109e2d0c8a59e1c2507c36273eac8
kedar pathak <kedar.pathak@pragtech.co.in>
2012-07-03 17:29:52 +0530

    need to merge

--- current/app/controllers/admin/artworks_controller.rb
+++ current/app/controllers/admin/artworks_controller.rb
@@ -78,27 +78,27 @@ class Admin::ArtworksController < ApplicationController
  
       if str[1]== "comp" 
         com = Commission.find(:first,:conditions=>["artworks_competition_id = ? and edition_no = ?",str[3],str[4]]) 
-       exit
+    
        if com.blank?
-        p "first time i will be here"
+         
        com = Commission.new(:artworks_competition_id=>str[3],:edition_no=>str[4])
-       p com.save
-       p "ssssssssss"
+       com.save
+       
        end
-       p com
+        
          if str[2]=="comm"    
             com.update_attribute('art_comm',params[:update_value])
          end
          if str[2]=="paid" and params[:update_value]=="paid"
             com.update_attribute('art_comm_paid',1)
           end
-         if
+         if str[2]=="paid" and params[:update_value]=="Unpaid"
             com.update_attribute('art_commission_paid',0)
          end
          p "i am here in com2"
-         exit 
+        
          if str[2]=="salesp"
-            com.update_attribute('sales_prsn',params[:update_value])
+            com.update_attribute('sales_person',params[:update_value])
          end
          
          if str[2]=="salespc"
@@ -106,9 +106,9 @@ class Admin::ArtworksController < ApplicationController
          end
          
          if str[2]=="salpcp" and params[:update_value]=="paid"
-            com.update_attribute('sales_person_comm_paid',1)
+            com.update_attribute('sales_person_commission_paid',1)
          end
-         if
+         if str[2]=="salpcp" and params[:update_value]=="Unpaid"
             com.update_attribute('sales_person_commission_paid',0)
          end
      end
